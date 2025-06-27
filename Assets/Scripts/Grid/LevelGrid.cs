7a859b1dc6ba0322834b79503502b0a7a017c76d
RYpLi <zjanasek27@gmail.com>
2025-06-27 03:18:25 +0200

    GridShowup

--- Assets/Scripts/Grid/LevelGrid.cs
+++ Assets/Scripts/Grid/LevelGrid.cs
@@ -47,12 +47,14 @@ public class LevelGrid : MonoBehaviour
         AddUnitAtGridPosition(toGridPosition, unit);
     }
 
-    // Ta szczalka to jest dolownie Funkcja { return (zawartosc =>);}
 
+    // Ta szczalka to jest dolownie Funkcja { return (zawartosc =>);}
     public GridPosition GetGridPosition(Vector3 worldPosition) => gridSystem.GetGridPosition(worldPosition);
     public Vector3 GetWorldPosition(GridPosition gridPosition) => gridSystem.GetWorldPosition(gridPosition);
-
     public bool IsValidGridPosition(GridPosition gridPosition) => gridSystem.IsValidGridPosition(gridPosition);
+    public int GetWidth() => gridSystem.GetWidth();
+    public int GetHeight() => gridSystem.GetHeight();
+
 
     public bool HasAnyUnitOnGridPosition(GridPosition gridPosition)
     {
